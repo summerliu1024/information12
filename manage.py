87f7652cbda6eaff562182c0e210541686a065e1
summer <948605548@qq.com>
2019-06-08 14:09:35 +0800

    抽取Config类到单独的文件config.py中

--- manage.py
+++ manage.py
@@ -14,30 +14,7 @@ from flask_wtf import CSRFProtect
 from flask_session import Session
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-
-
-class Config(object):
-    SECRET_KEY = 'djfdshlfkhsdakfhasdklfhkpas;d'
-    # 调试模式
-    DEBUG = True
-    # 数据库连接
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1/information12'
-    # 数据库跟踪
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    # redis ip和端口配置
-    REDIS_HOST = "127.0.0.1"
-    REDIS_PORT = "6379"
-    # flask-session的配置信息
-    SESSION_TYPE = 'redis'
-    # 指定存储session的存储对象
-    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
-    # 设置session签名
-    SESSION_USE_SIGNER = True
-    # 设置session永久保存与否
-    SESSION_PERMANENT = False
-    # 设置session的保存时间
-    PERMANENT_SESSION_LIFETIME = 86400 * 2
-
+from config import Config
 
 app = Flask(__name__)
 
