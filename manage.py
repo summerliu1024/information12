568f031d2b252207c6a500a36efea9645b4370af
summer <948605548@qq.com>
2019-06-08 11:52:58 +0800

    集成redis

--- manage.py
+++ manage.py
@@ -1,8 +1,11 @@
 """一，集成配置类
-    二，sqlalchemy"""
+    二，sqlalchemy
+    三，集成redis
+    """
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from redis import StrictRedis
 
 
 class Config(object):
@@ -12,6 +15,9 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1/information12'
     # 数据库跟踪
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    # redis ip和端口配置
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = "6379"
 
 
 app = Flask(__name__)
@@ -20,10 +26,13 @@ app = Flask(__name__)
 app.config.from_object(Config)
 # 集成Sqlalchemy到flask
 db = SQLAlchemy(app)
+# 连接redis
+redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 
 
 @app.route('/')
 def index():
+    redis_store.set("name", "summer")
     return 'index'
 
 
