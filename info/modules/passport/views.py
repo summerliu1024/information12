56a8c8ff45e04d0ee52b7f299de78bbf33f25994
summer <948605548@qq.com>
2019-06-08 17:19:04 +0800

    图片验证码后端逻辑实现

--- info/modules/passport/views.py
+++ info/modules/passport/views.py
@@ -1,5 +1,31 @@
+from flask import request, abort, current_app, make_response
+
+from info import redis_store, constants
 from info.modules.passport import passport_blu
+from info.utils.captcha.captcha import captcha
+
 
 @passport_blu.route('/image_code')
 def get_image_conde():
-    return "image_code"
+    # 1. 取到参数
+    # args: 取到url中 ? 后面的参数
+    image_code_id = request.args.get("imageCodeId", None)
+    # 2. 判断参数是否有值
+    if not image_code_id:
+        return abort(403)
+
+    # 3. 生成图片验证码
+    name, text, image = captcha.generate_captcha()
+
+    # 4. 保存图片验证码文字内容到redis
+    try:
+        redis_store.set("ImageCodeId_" + image_code_id, text, constants.IMAGE_CODE_REDIS_EXPIRES)
+    except Exception as e:
+        current_app.logger.error(e)
+        abort(500)
+
+    # 5. 返回验证码图片
+    response = make_response(image)
+    # 设置数据的类型，以便浏览器更加智能识别其是什么类型
+    response.headers["Content-Type"] = "image/jpg"
+    return response
