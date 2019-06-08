92eff6bedcea6248365de21dcdd42db8f94eb89f
summer <948605548@qq.com>
2019-06-08 13:42:31 +0800

    项目集成flask-session 并配置session保存到redis的相关配置

--- manage.py
+++ manage.py
@@ -2,12 +2,14 @@
     二，sqlalchemy
     三，集成redis
     四，集成csrfprotect
+    五，集成flask-session
     """
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
 from flask_wtf import CSRFProtect
+from flask_session import Session
 
 
 class Config(object):
@@ -20,6 +22,16 @@ class Config(object):
     # redis ip和端口配置
     REDIS_HOST = "127.0.0.1"
     REDIS_PORT = "6379"
+    # flask-session的配置信息
+    SESSION_TYPE = 'redis'
+    # 指定存储session的存储对象
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 设置session签名
+    SESSION_USE_SIGNER = True
+    # 设置session永久保存与否
+    SESSION_PERMANENT = False
+    # 设置session的保存时间
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
 
 
 app = Flask(__name__)
@@ -33,6 +45,10 @@ redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 
 # CSRFPROTECT保护
 CSRFProtect(app)
+
+# 集成flask-session
+# 说明 flask中的session是保存用户数据的容器（上下文），而flask-session是指定session保存的路径
+Session(app)
 
 
 @app.route('/')
