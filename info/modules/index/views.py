443d6d237e9d234e45a3bdd68bbd5e7965ab3ab3
summer <948605548@qq.com>
2019-06-08 16:34:17 +0800

    处理浏览器图标显示

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, redirect, current_app
 
 from info import redis_store
 from info.modules.index import index_blu
@@ -7,3 +7,9 @@ from info.modules.index import index_blu
 @index_blu.route('/')
 def index():
     return render_template("news/index.html")
+
+
+@index_blu.route("/favicon.ico")
+def favicon():
+    # return redirect("/static/news/favicon.ico")
+    return current_app.send_static_file("news/favicon.ico")
