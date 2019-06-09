39e4acc449e7ac841070c237b60d6f28e1541039
summer <948605548@qq.com>
2019-06-09 11:13:05 +0800

    右上角登录图标后端逻辑

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,12 +1,33 @@
-from flask import render_template, redirect, current_app
+from flask import render_template, redirect, current_app, session
 
 from info import redis_store
+from info.models import User
 from info.modules.index import index_blu
 
 
 @index_blu.route('/')
 def index():
-    return render_template("news/index.html")
+    """
+    显示首页
+    1. 如果用户已经登录，将当前登录用户的数据传到模板中，供模板显示
+    :return:
+    """
+
+    # 取到用户id
+    user_id = session.get("user_id", None)
+    user = None
+    if user_id:
+        # 尝试查询用户的模型
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
+
+    data = {
+        "user": user.to_dict() if user else None
+    }
+
+    return render_template('news/index.html', data=data)
 
 
 @index_blu.route("/favicon.ico")
