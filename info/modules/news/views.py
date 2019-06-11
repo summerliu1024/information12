793458ec0cd2040bc18e74e0642b45a54ceab8b3
summer <948605548@qq.com>
2019-06-11 15:41:50 +0800

    基类模版抽取

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -5,4 +5,8 @@ from info.modules.news import news_blu
 
 @news_blu.route('/<int:news_id>')
 def news_detail(news_id):
-    return render_template('news/detail.html')
+    data = {
+
+    }
+
+    return render_template("news/detail.html", data=data)
