fb8ce47b0517a294891be1aaac23462da8851a96
summer <948605548@qq.com>
2019-06-11 16:07:51 +0800

    详情页用户信息显示

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,12 +1,21 @@
-from flask import render_template
+from flask import render_template, session, current_app
 
+from info.models import User
 from info.modules.news import news_blu
 
 
 @news_blu.route('/<int:news_id>')
 def news_detail(news_id):
+    # 查询用户登录信息
+    user_id = session.get("user_id", None)
+    user = None
+    if user_id:
+        # 尝试查询用户的模型
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
     data = {
-
+        "user": user.to_dict() if user else None,
     }
-
-    return render_template("news/detail.html", data=data)
+    return render_template('news/detail.html', data=data)
