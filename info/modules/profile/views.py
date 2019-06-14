bc4b0419a79242bc9b861d3f6227bdb966e2abf0
summer <948605548@qq.com>
2019-06-14 14:37:39 +0800

    修改个人信息后端实现

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,18 +1,36 @@
-from flask import render_template, g
+from flask import render_template, g, request, jsonify
 from werkzeug.utils import redirect
 
 from info.modules.profile import profile_blu
 from info.utils.common import user_login_data
+from info.utils.response_code import RET
 
 
-@profile_blu.route('/base_info')
+@profile_blu.route('/base_info', methods=["GET", "POST"])
 @user_login_data
 def base_info():
-    """
-    用户基本信息
-    :return:
-    """
-    return render_template('news/user_base_info.html', data={"user": g.user.to_dict()})
+    # 不同的请求方式，做不同的事情
+    if request.method == "GET":
+        return render_template('news/user_base_info.html', data={"user": g.user.to_dict()})
+
+    # 代表是修改用户数据
+    # 1. 取到传入的参数
+    nick_name = request.json.get("nick_name")
+    signature = request.json.get("signature")
+    gender = request.json.get("gender")
+
+    # 2. 校验参数
+    if not all([nick_name, signature, gender]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+    if gender not in ("WOMAN", "MAN"):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    user = g.user
+    user.signature = signature
+    user.nick_name = nick_name
+    user.gender = gender
+
+    return jsonify(errno=RET.OK, errmsg="OK")
 
 
 @profile_blu.route('/info')
