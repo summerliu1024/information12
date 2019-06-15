7103f1ddcf63847bc98b4b1072935f2eb841e364
summer <948605548@qq.com>
2019-06-15 14:36:42 +0800

    创建管理员账户

--- manage.py
+++ manage.py
@@ -22,6 +22,29 @@ manager = Manager(app)
 Migrate(app, db)
 manager.add_command("db", MigrateCommand)
 
+
+@manager.option('-n', '--name', dest="name")
+@manager.option('-p', '--password', dest="password")
+def createsuperuser(name, password):
+
+    if not all([name, password]):
+        print("参数不足")
+
+    user = User()
+    user.nick_name = name
+    user.mobile = name
+    user.password = password
+    user.is_admin = True
+
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(e)
+
+    print("添加成功")
+
 if __name__ == '__main__':
     # 运行当前Flask应用程序
     manager.run()
