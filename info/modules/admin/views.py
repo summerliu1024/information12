e87fb9fd6251472947ff7e110e99066f8d199e01
summer <948605548@qq.com>
2019-06-15 15:07:04 +0800

    管理员登录未渲染首页

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -1,8 +1,41 @@
-from flask import render_template
+from flask import render_template, request, current_app, session
 
+from info.models import User
 from info.modules.admin import admin_blu
 
 
-@admin_blu.route('/login')
+@admin_blu.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template('admin/login.html')
+    if request.method == "GET":
+        return render_template('admin/login.html')
+
+    # 取到登录的参数
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    # 判断参数
+    if not all([username, password]):
+        return render_template('admin/login.html', errmsg="参数错误")
+
+    # 查询当前用户
+    try:
+        user = User.query.filter(User.mobile == username, User.is_admin == True).first()
+    except Exception as e:
+        current_app.logger.error(e)
+        return render_template('admin/login.html', errmsg="用户信息查询失败")
+
+    if not user:
+        return render_template('admin/login.html', errmsg="未查询到用户信息")
+
+    # 校验密码
+    if not user.check_passowrd(password):
+        return render_template('admin/login.html', errmsg="用户名或者密码错误")
+
+    # 保存用户的登录信息
+    session["user_id"] = user.id
+    session["mobile"] = user.mobile
+    session["nick_name"] = user.nick_name
+    session["is_admin"] = user.is_admin
+
+    # TODO 跳转到后台管理主页,暂未实现
+    return "登录成功，需要跳转到主页"
